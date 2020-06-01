5dc7880bbf83dab6b271573dfe1e7a3f4a45dcf2
WebTerminator <alessandro.santese@wearedauntless.com>
2020-06-01 21:06:25 +0100

    clean up

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import Link from 'next/link'
-import { Navigation, NavLink } from './style'
+import React from "react";
+import Link from "next/link";
+import { Navigation, NavLink } from "./style";
 import { useRouter } from "next/router";
 
-
-// import CATEGORIES_QUERY from '../apollo/queries/category/categories';
-
 const Nav = () => {
-  const router = useRouter()
-  const { route } = router
+  const router = useRouter();
+  const { route } = router;
 
   return (
     <Navigation>
-      <Link href='/'>
-        <NavLink className={route === '/' ? 'active' : ''}>home</NavLink>
+      <Link href="/">
+        <NavLink className={route === "/" ? "active" : ""}>home</NavLink>
       </Link>
-      <Link href='/blog'>
-        <NavLink className={route.startsWith('/blog') ? 'active' : ''}>blog</NavLink>
+      <Link href="/blog">
+        <NavLink className={route.startsWith("/blog") ? "active" : ""}>
+          blog
+        </NavLink>
       </Link>
     </Navigation>
   );
