9131c8ad11ef23bc85d816ba06fa9f933e9cb736
WebTerminator <alessandro.santese@wearedauntless.com>
2020-06-05 18:16:41 +0100

    profile url updated

--- components/Main/index.js
+++ components/Main/index.js
@@ -3,13 +3,15 @@ import { Title } from "../Style";
 import { ProfileImg, Wrapper } from "./style";
 import SideBarShare from "../SideBarShare";
 import Cv from "../Cv";
-import { BASE_API_URL } from "../../api";
+import { BASE_API_URL, isDevelopment } from "../../api";
 
 const Main = ({ profile }) => {
+  const url = isDevelopment ? `${BASE_API_URL}${profile.url}` : profile.url;
+
   return (
     <Wrapper>
       <ProfileImg>
-        <img src={`${BASE_API_URL}${profile.url}`} />
+        <img src={url} />
       </ProfileImg>
       <Title>
         <span>Alessandro Santese is a Sutton based </span>
