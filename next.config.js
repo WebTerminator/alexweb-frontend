168a6d3ec8ed37413b0b09840619d8a9b49c88a7
Alessandro <as-web@hotmail.com>
2022-07-01 10:14:48 +0100

    version upddated

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 require("dotenv").config();
-const withCSS = require("@zeit/next-css");
 
-module.exports = withCSS({
+module.exports = {
   env: {
     API_URL: process.env.API_URL,
     API_URL_PROD: process.env.API_URL_PROD,
-  },
-});
+  }
+}
